ce8d6498985ababa22a8705fff249f9254dd97e8
Elam Day <elam.day.friedland@gmail.com>
2021-01-12 15:47:57 -0800

    fixed times, added names

--- app.js
+++ app.js
@@ -38,40 +38,35 @@ app.get('/', function(req, res){
 
 
 app.post("/in", function (req, res) {
-	con.query(`INSERT INTO sensdata (id,temp) VALUES (${req.body.id}, ${req.body.temp})`, function (err, result){
+	con.query(`INSERT INTO sensdata (id, name, temp, humidity, pressure, altitude) VALUES (${req.body.id}, "${req.body.name}", ${req.body.temp}, ${req.body.humidity}, ${req.body.pressure}, ${req.body.altitude})`, function (err, result){
 		if (err){
 			throw err;
 			res.sendStatus(500);
 		}
 		updateData();
 	});
-	// con.query('SELECT * from sensdata', function (err, result){
-	// con.query('SELECT * FROM sensdata WHERE id = ?', [id], function (err, results) { // maybe useful for individual sensor graphs
-	// 	if (err) throw err;
-	// 	console.log(result);
-	// });
 	res.sendStatus(200);
 })
 io.sockets.on('connection', function (socket) {
 	var id = 1
-	con.query('SELECT DISTINCT id from sensdata', function (err, results){
+	con.query('SELECT DISTINCT id, name from sensdata', function (err, results){
 			if (err) throw err;
 			results.forEach(elem =>{
-				con.query('SELECT time, temp FROM sensdata WHERE id = ?', [elem.id], function (err, results) {
+				con.query('SELECT UNIX_TIMESTAMP(time), temp, humidity, pressure, altitude, name FROM sensdata WHERE id = ?', [elem.id], function (err, results) {
 					if (err) throw err;
-					socket.emit("chartinit", {data:results.map(Object.values), id:elem.id})
+					socket.emit("chartinit", {data:results.map(Object.values), id:elem.id, name:elem.name})
 			});
 		});
 	});
 });
 
 function updateData() {
-	con.query('SELECT DISTINCT id from sensdata', function (err, results){
+	con.query('SELECT DISTINCT id, name from sensdata', function (err, results){
 			if (err) throw err;
 			results.forEach(elem =>{
-				con.query("SELECT time, temp FROM sensdata WHERE id = ? ORDER BY time DESC LIMIT 1", [elem.id], function (err, result) {
+				con.query("SELECT UNIX_TIMESTAMP(time), temp, humidity, pressure,altitude FROM sensdata WHERE id = ? ORDER BY time DESC LIMIT 1", [elem.id], function (err, results) {
 					if (err) throw err;
-						io.emit("update", {data:result.map(Object.values), id:elem.id});
+						io.emit("update", {data:results.map(Object.values), id:elem.id, name:elem.name});
 			});
 		});
 	});
