8613059032ffc46bbd776b9d3b873947fef47596
Elam Day <elam.day.friedland@gmail.com>
2021-01-11 17:47:07 -0800

    live updates with multiple ids

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.engine("handlebars", handlebars({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({ lextended:true}));
 var port = 3257;
+var novel = 0;
 /* set up sql con */
 var mysql = require("mysql");
 var con = mysql.createConnection({
@@ -39,7 +40,11 @@ app.get('/', function(req, res){
 
 app.post("/in", function (req, res) {
 	con.query(`INSERT INTO sensdata (id,temp) VALUES (${req.body.id}, ${req.body.temp})`, function (err, result){
-		if (err) throw err;
+		if (err){
+			throw err;
+			res.sendStatus(500);
+		}
+		novel = 1;
 	});
 	// con.query('SELECT * from sensdata', function (err, result){
 	// con.query('SELECT * FROM sensdata WHERE id = ?', [id], function (err, results) { // maybe useful for individual sensor graphs
@@ -55,11 +60,27 @@ io.sockets.on('connection', function (socket) {
 			results.forEach(elem =>{
 				con.query('SELECT time, temp FROM sensdata WHERE id = ?', [elem.id], function (err, results) {
 					if (err) throw err;
-					socket.emit("chartinit", {data:results.map(Object.values), id:"Sensor " +elem.id})
+					socket.emit("chartinit", {data:results.map(Object.values), id:elem.id})
 			});
 		});
 	});
 });
+
+var recent = {}
+setInterval(function() {
+	con.query('SELECT DISTINCT id from sensdata', function (err, results){
+			if (err) throw err;
+			results.forEach(elem =>{
+				con.query("SELECT time, temp FROM sensdata WHERE id = ? ORDER BY time DESC LIMIT 1", [elem.id], function (err, result) {
+					if (err) throw err;
+					if (novel){
+						io.emit("update", {data:result.map(Object.values), id:elem.id})
+						novel = 0;
+					}
+			});
+		});
+	});
+}, 1000);
 
 http.listen(port, function(){
 	console.log("app listening on port: " + port);
