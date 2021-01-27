52df69c72d8f56604117877b3c54ce5d86cc48b6
Elam Day <elam.day.friedland@gmail.com>
2021-01-27 14:04:45 -0800

    initally working

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
-var app = require("express")();
+var express = require("express")
+const bcrypt = require('bcrypt');
+var app = express();
+// var login = require('./routes/loginroutes');
 var bodyParser = require("body-parser");
 var urlencodedParser = bodyParser.urlencoded({extended: true});
 var handlebars = require("express-handlebars");
+var router = express.Router();
+var port = 80;
+const saltRounds = 10;
 const http = require('http').Server(app);
 const io = require('socket.io')(http, { transports: ['websocket', 'polling'], cookie:true, secure: true });
 app.engine("handlebars", handlebars({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({ lextended:true}));
-var port = 80;
+app.use(bodyParser.json())
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 /* set up sql con */
 var mysql = require("mysql");
 var con = mysql.createConnection({
@@ -20,13 +32,139 @@ var con = mysql.createConnection({
 var tries = 5;
 while (tries < 5){
 	con.connect(function(err){
+
 		if(err){
 			tries += 1
 			console.error("error connecting: " + err.stack);
+		}else{
+				if (err) throw err;
 		}
 	});
 	sleep(1000);
 }
+register = async function(req,res){
+    const password = req.body.password;
+    const encryptedPassword = await bcrypt.hash(password, saltRounds)
+    var creds={
+      "username":req.body.username,
+      "password":encryptedPassword
+    }
+
+  con.query('INSERT INTO users SET ?',creds, function (error, results, fields) {
+    if (error) {
+      res.send({
+        "code":400,
+        "failed":"error ocurred"
+      })
+    } else {
+      res.send({
+        "code":200,
+        "success":"user registered sucessfully"
+          });
+      }
+  });
+};
+regsens = async function(req, res) {
+	con.query("SHOW TABLES", function (err, results) {
+		console.log(results)
+	});
+	var sensorname = req.body.sensorname
+	var username = req.body.username
+	var password = req.body.password
+  con.query('SELECT * FROM users WHERE username = ?',[username], async function (error, results, fields) {
+    if (error) {
+      res.send({
+        "code":400,
+        "failed":"error ocurred"
+      })
+    }else{
+      if(results[0] != undefined){ // results[0] sometimes still undef if no user even if this passes
+        const comparision = await bcrypt.compare(password, results[0].password)
+        if(comparision) {
+		  con.query('SELECT * FROM sensors WHERE sensorname = ?',[sensorname], async function (error, results, fields) {
+			  if (results[0] != undefined ) {
+				  res.send({
+					  "code":206,
+					  "success":"sensor name is not unique"
+				  })
+			  } else {
+				var newsens = {
+					  "belongsto":username,
+					  "sensorname": sensorname
+				}
+				con.query('INSERT into sensors SET ?',[newsens], async function (err, results, fields) {
+					if (err){
+					      res.send({
+						"code":400,
+						"failed":"error ocurred"
+					      })
+						console.log(error)
+					} else {
+						res.send({
+						"code":200,
+						"success":"user registered sucessfully"
+						});
+					}
+				});
+			  }
+        	});
+	}
+        else{
+          res.send({
+               "code":204,
+               "success":"Username and password does not match"
+          })
+        }
+      }
+      else{
+        res.send({
+          "code":206,
+          "success":"Username does not exist"
+            });
+      }
+    }
+    });
+}
+
+login = async function(req,res){
+  var username= req.body.username;
+  var password = req.body.password;
+  con.query('SELECT * FROM users WHERE username = ?',[username], async function (error, results, fields) {
+    if (error) {
+      res.send({
+        "code":400,
+        "failed":"error ocurred"
+      })
+    }else{
+      if(results[0] != undefined){
+        const comparision = await bcrypt.compare(password, results[0].password)
+        if(comparision){
+            res.send({
+              "code":200,
+              "success":"login sucessfull"
+            })
+        }
+        else{
+          res.send({
+               "code":204,
+               "success":"Username and password does not match"
+          })
+        }
+      }
+      else{
+        res.send({
+          "code":206,
+          "success":"Username does not exits"
+            });
+      }
+    }
+    });
+}
+
+router.post('/register',register);
+router.post('/login',login);
+router.post('/regsens',regsens);
+app.use('/api', router);
 app.get('/', function(req, res){
 	res.sendFile("index.html", {root:__dirname})
 });
@@ -41,7 +179,7 @@ app.get("/test", function(req, res){
 });
 
 app.post("/checkUnique", function(req, res){
-	con.query(`SELECT COUNT(name) AS n FROM sensdata WHERE name = ? LIMIT 1`, [req.body.name], function(err, result){
+	con.query(`SELECT COUNT(sensorname) AS n FROM sensdata WHERE sensorname = ? LIMIT 1`, [req.body.name], function(err, result){
 		if (err) throw err;
 		if (!result[0].n)
 			res.sendStatus(200);
@@ -52,32 +190,77 @@ app.post("/checkUnique", function(req, res){
 
 
 app.post("/in", function (req, res) {
-		req.body.temp = (req.body.temp === undefined) ? null : req.body.temp; // there's gotta be a better way to do this but I don't want to if statement through the possible sensor configurations and do a special query for each so here we are
-		req.body.humidity = (req.body.humidity === undefined) ? null : parseFloat(req.body.humidity);
-		req.body.pressure = (req.body.pressure === undefined) ? null : parseFloat(req.body.pressure);
-		req.body.altitude = (req.body.altitude === undefined) ? null : parseFloat(req.body.altitude);
-		req.body.NO2 = (req.body.NO2 === undefined) ? null : parseFloat(req.body.NO2);
-		req.body.NH3 = (req.body.NH3 === undefined) ? null : parseFloat(req.body.NH3);
-		req.body.CO = (req.body.CO === undefined) ? null : parseFloat(req.body.CO);
-		req.body.CO2 = (req.body.CO2 === undefined) ? null : parseFloat(req.body.CO2);
-		con.query(`INSERT INTO sensdata (name, temp, humidity, pressure, altitude, NO2, NH3, CO, CO2) VALUES ("${req.body.name}", ${req.body.temp}, ${req.body.humidity}, ${req.body.pressure}, ${req.body.altitude}, ${req.body.NO2}, ${req.body.NH3}, ${req.body.CO}, ${req.body.CO2})`, function (err, result){
-			if (err){
-				res.sendStatus(500);
-				throw err;
-			} else {
-			updateData();
-			res.sendStatus(200)
-			}
-		});
+
+	var sensorname = req.body.sensorname
+	var username = req.body.username
+	var password = req.body.password
+	req.body.temp = (req.body.temp === undefined) ? null : req.body.temp; // there's gotta be a better way to do this but I don't want to if statement through the possible sensor configurations and do a special query for each so here we are
+	req.body.humidity = (req.body.humidity === undefined) ? null : parseFloat(req.body.humidity);
+	req.body.pressure = (req.body.pressure === undefined) ? null : parseFloat(req.body.pressure);
+	req.body.altitude = (req.body.altitude === undefined) ? null : parseFloat(req.body.altitude);
+	req.body.NO2 = (req.body.NO2 === undefined) ? null : parseFloat(req.body.NO2);
+	req.body.NH3 = (req.body.NH3 === undefined) ? null : parseFloat(req.body.NH3);
+	req.body.CO = (req.body.CO === undefined) ? null : parseFloat(req.body.CO);
+	req.body.CO2 = (req.body.CO2 === undefined) ? null : parseFloat(req.body.CO2);
+  con.query('SELECT * FROM users WHERE username = ?',[username], async function (error, results, fields) {
+    if (error) {
+      res.send({
+        "code":400,
+        "failed":"error ocurred"
+      })
+    }else{
+      if(results[0] != undefined){
+        const comparision = await bcrypt.compare(password, results[0].password)
+        if(comparision){
+		  con.query('SELECT * FROM sensors WHERE sensorname = ?',[sensorname], async function (error, results, fields) {
+			  if (results[0] == undefined) {
+				  res.send({
+					  "code":206,
+					  "success":"sensor doesn't exist"
+				  })
+			  } else {
+				con.query(`INSERT INTO sensdata (sensorname, temp, humidity, pressure, altitude, NO2, NH3, CO, CO2) VALUES ("${req.body.sensorname}", ${req.body.temp}, ${req.body.humidity}, ${req.body.pressure}, ${req.body.altitude}, ${req.body.NO2}, ${req.body.NH3}, ${req.body.CO}, ${req.body.CO2})`, function (err, result){
+				if (err){
+					  res.send({
+						  "code":206,
+						  "success":"data insert failed"
+					  })
+					throw err;
+				} else {
+					updateData();
+					res.send({
+						"code":200,
+						"success":"data pushed!"
+					})
+				}
+        	});
+	}
+        	});
+      }
+        else{
+          res.send({
+               "code":204,
+               "success":"Username and password does not match"
+          })
+        }
+      }
+      else{
+        res.send({
+          "code":206,
+          "success":"Username does not exist"
+            });
+      }
+    }
+    });
 });
 
 function initGraphs(socket){
-	con.query('SELECT DISTINCT name from sensdata', function (err, results){
+	con.query('SELECT DISTINCT sensorname from sensdata', function (err, results){
 			if (err) throw err;
 			results.forEach(elem =>{
-				con.query(`SELECT UNIX_TIMESTAMP(time) * 1000, temp, humidity, CO2, NO2, NH3, CO, pressure, altitude, name FROM sensdata WHERE name = ?`, [elem.name], function (err, results) {
+				con.query(`SELECT UNIX_TIMESTAMP(time) * 1000, temp, humidity, CO2, NO2, NH3, CO, pressure, altitude, sensorname FROM sensdata WHERE sensorname = ?`, [elem.sensorname], function (err, results) {
 					if (err) throw err;
-					socket.emit("chartInit", {data:results.map(Object.values), id:elem.name, name:elem.name})
+					socket.emit("chartInit", {data:results.map(Object.values), id:elem.sensorname, name:elem.sensorname})
 			});
 		});
 	});
@@ -86,17 +269,17 @@ function initGraphs(socket){
 function initAverages(socket){ // TODO make the averages not just alltime
 	con.query(`SELECT AVG(temp), AVG(humidity), AVG(pressure), AVG(CO2) FROM sensdata`, function (err, results){
 		if (err) throw err;
-		socket.emit("averagesInit", {data:results.map(Object.values)[0], id:"all", name:"all"});
+		socket.emit("averagesInit", {data:results.map(Object.values)[0], id:"all", sensorname:"all"});
 	});
 }
 
 function updateData() {
-	con.query('SELECT DISTINCT name from sensdata', function (err, results){
+	con.query('SELECT DISTINCT sensorname from sensdata', function (err, results){
 			if (err) throw err;
 			results.forEach(elem =>{
-				con.query("SELECT UNIX_TIMESTAMP(time) * 1000, temp, humidity, CO2, NO2, NH3, CO, pressure, altitude, name FROM sensdata WHERE name = ? ORDER BY time DESC LIMIT 1", [elem.name], function (err, results) {
+				con.query("SELECT UNIX_TIMESTAMP(time) * 1000, temp, humidity, CO2, NO2, NH3, CO, pressure, altitude, sensorname FROM sensdata WHERE sensorname = ? ORDER BY time DESC LIMIT 1", [elem.sensorname], function (err, results) {
 					if (err) throw err;
-						io.emit("chartUpdate", {data:results.map(Object.values), id:elem.name, name:elem.name});
+						io.emit("chartUpdate", {data:results.map(Object.values), id:elem.sensorname, name:elem.sensorname});
 			});
 		});
 	});
--- posttest.py
+++ posttest.py
@@ -3,9 +3,21 @@ import random
 import requests
 # host = "https://aqclub-service-tazpkivgxq-uw.a.run.app/in"
 host = "http://127.0.0.1/in"
+rhost = "http://127.0.0.1/api/register"
+lhost = "http://127.0.0.1/api/login"
+shost = "http://127.0.0.1/api/regsens"
 
-while True:
-    print(requests.post(host, data={'id':1, "name":"bruh", "temp":random.randint(24,35), "humidity":random.randint(30,80), "pressure":random.randint(950,1150), "altitude":random.randint(95, 115), "NO2":random.randint(1,10), "CO2":random.randint(1,10), "NH3":random.randint(1,10), "CO":random.randint(1,10)}))
-    # print(requests.post(host, data={'id':2, "name":"DavidHouse", "temp":random.randint(24,35), "humidity":random.randint(30,80), "pressure":random.randint(950,1150), "altitude":random.randint(95, 115)}))
-    # print(requests.post(host, data={'id':3, "name":"AHS", "temp":random.randint(24,35), "humidity":random.randint(30,80), "pressure":random.randint(950,1150), "altitude":random.randint(95, 115)}))
-    time.sleep(5)
+name = "elam1"
+passwd = "bruh123"
+sensname = "test1"
+
+def register():
+    print(requests.post(rhost, data={"username":name, "password":passwd}).text)
+def login():
+    print(requests.post(lhost, data={"username":name, "password":passwd}).text)
+def newsens():
+    print(requests.post(shost, data={"sensorname":sensname, "username": name, "password":passwd}).text)
+
+
+def send_data():
+        print(requests.post(host, data={"sensorname":sensname, "username":name, "password":passwd, "temp":random.randint(24,35), "humidity":random.randint(30,80), "pressure":random.randint(950,1150), "altitude":random.randint(95, 115), "NO2":random.randint(1,10), "CO2":random.randint(1,10), "NH3":random.randint(1,10), "CO":random.randint(1,10)}).text)
